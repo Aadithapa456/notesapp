b34b4d210a6bcc5ba82800fc0f37376694e43161
Aaditya <aadityathapa977@gmail.com>
2024-09-07 19:32:01 +0545

    Added notes, about section, localstorage for theme

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function themeChanger() {
       variables.style.setProperty("--theme", "#121212");
       variables.style.setProperty("--text-color", "white");
       variables.style.setProperty("--nav-theme", "#121212");
-
+      localStorage.setItem("theme", "dark"); // Saves Dark Theme in local storage
    } else {
       variables.style.setProperty(
          "--theme",
@@ -20,9 +20,25 @@ function themeChanger() {
       );
       variables.style.setProperty("--text-color", "black");
       variables.style.setProperty("--nav-theme", "#e3e3e355");
-
+      localStorage.setItem("theme", "light"); //Saves light theme in local storage
    }
 }
+function loadTheme() {
+   const savedTheme = localStorage.getItem("theme"); // Extracts the stored theme from localstorage
+   if (savedTheme === "dark") {
+      variables.style.setProperty("--theme", "#121212");
+      variables.style.setProperty("--text-color", "white");
+      variables.style.setProperty("--nav-theme", "#121212");
+   } else {
+      variables.style.setProperty(
+         "--theme",
+         "radial-gradient(343px at 46.3% 47.5%, rgb(242, 242, 242) 0%, rgb(241, 241, 241) 72.9%)"
+      );
+      variables.style.setProperty("--text-color", "black");
+      variables.style.setProperty("--nav-theme", "#e3e3e355");
+   }
+}
+document.addEventListener("DOMContentLoaded", loadTheme); // Runs the loadTheme() function everytime the page loads
 function navBar() {
    let nav = document.querySelector(".nav-items");
    let isVisible = nav.classList.contains("nav-visible");
